bd113100814e6d98a07992f6de207e3543e8c388
Darshana Mahesh <darshanamahesh1600@gmail.com>
2025-07-31 14:06:44 +0530

    Upodated authentication part added register and login pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+import Booking from './Booking';
+
 
 function App() {
   const [name, setName] = useState('');
@@ -19,6 +24,18 @@ function App() {
 
   return (
     <div>
+      <Router>
+        <nav>
+          <Link to="/register">Register</Link> |{' '}
+          <Link to="/login">Login</Link> |{' '}
+          <Link to="/book">Book</Link>
+        </nav>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/book" element={<Booking />} />
+        </Routes>
+      </Router>
       <h1>Home Service Booking</h1>
       <form onSubmit={handleSubmit}>
         <input type="text" placeholder="Your Name" value={name} onChange={e => setName(e.target.value)} required /><br />
